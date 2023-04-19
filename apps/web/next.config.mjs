765cfee8e2f5bc877618da1b9935a8014cd09774
rabbit <brownrabbit0207@gmail.com>
2023-04-19 18:59:23 +0900

    homepage is shown

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -111,11 +111,6 @@ const config = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/swap',
-        permanent: true,
-      },
-      {
         source: '/send',
         destination: '/swap',
         permanent: true,
