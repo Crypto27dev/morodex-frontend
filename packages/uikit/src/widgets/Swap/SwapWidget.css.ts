15b6fde684202e141933498371aaf7824b5fda1d
rabbit <brownrabbit0207@gmail.com>
2023-04-21 07:27:05 +0900

    One error is fixed.

--- packages/uikit/src/widgets/Swap/SwapWidget.css.ts
+++ packages/uikit/src/widgets/Swap/SwapWidget.css.ts
@@ -18,12 +18,20 @@ export const iconDownClass = style({
   selectors: {
     [`${switchButtonClass}:hover &`]: {
       display: "none",
+      fill: "rgba(255, 255, 255, 0.7)",
+      borderRadius: "50%",
+      boxShadow: "0 0 0 2px rgba(255, 255, 255, 0.7)",
+      color: "rgba(255, 255, 255, 0.7)",
+      width: "2.5em",
     },
   },
 });
 
 export const iconUpDownClass = style({
   display: "none",
+  color: "rgb(0, 233, 177)",
+  fill: "rgb(0, 233, 177) !important",
+  width: "2.5em",
   selectors: {
     [`${switchButtonClass}:hover &`]: {
       display: "block",
