b6caee2f7e08c527ca79f596a13ca006ff6d63c8
rabbit <brownrabbit0207@gmail.com>
2023-04-23 23:56:37 +0900

    fixed.

--- apps/web/src/views/Home/index.tsx
+++ apps/web/src/views/Home/index.tsx
@@ -48,7 +48,7 @@ const Home: React.FC<React.PropsWithChildren> = () => {
     const inputUsdAmount = e.target.value;
     const calcGainAmount = inputUsdAmount * (0.003 - 0.0007);
     setUsdAmount(inputUsdAmount);
-    setGainAmount(calcGainAmount);
+    setGainAmount(calcGainAmount.toFixed(2));
   }
 
   return (
@@ -3997,7 +3997,7 @@ const Home: React.FC<React.PropsWithChildren> = () => {
                 </a>
               </div>
               <div className="mt-8 ml-3 inline-block">
-                <a href="https://paladinsec.co/projects/smardex/" target="_blank"
+                <a href="#" target="_blank"
                   rel="noreferrer" className="flex items-center space-x-1 rounded bg-8c6fdf px-2 py-1 text-white">
                   <div className="relative"><svg aria-hidden="true" focusable="false" data-prefix="fas"
                     data-icon="shield-halved"
@@ -4541,14 +4541,16 @@ const Home: React.FC<React.PropsWithChildren> = () => {
               className="flex flex-col space-y-2 overflow-hidden rounded-xl border border-white-05 bg-transparent py-2 shadow-xl shadow-black backdrop-blur">
               <div className="inline-flex flex-none items-center justify-center space-x-2">
                 <img
-                  className="h-10 w-10 rounded-full bg-white-a25 p-1" src="/assets/images/tokens/sdex.svg"
-                  alt="MORODEX logo" /><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bolt"
+                  className="h-10 w-10 rounded-full bg-white-a25 p-1" src="/images/moro.svg"
+                  alt="MORODEX logo" />
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bolt"
                     className="svg-inline--fa fa-bolt fa-lg animate-pulse text-white" role="img"
                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
                   <path fill="currentColor"
                     d="M240.5 224H352C365.3 224 377.3 232.3 381.1 244.7C386.6 257.2 383.1 271.3 373.1 280.1L117.1 504.1C105.8 513.9 89.27 514.7 77.19 505.9C65.1 497.1 60.7 481.1 66.59 467.4L143.5 288H31.1C18.67 288 6.733 279.7 2.044 267.3C-2.645 254.8 .8944 240.7 10.93 231.9L266.9 7.918C278.2-1.92 294.7-2.669 306.8 6.114C318.9 14.9 323.3 30.87 317.4 44.61L240.5 224z">
                   </path>
-                </svg><img className="h-10 w-10 rounded-full bg-white-a25 p-1" src="/assets/images/tokens/uni.svg"
+                </svg>
+                <img className="h-10 w-10 rounded-full bg-white-a25 p-1" src="/images/uni.svg"
                   alt="Uniswap logo" /></div>
               <div className="grow">
                 {/* <LogChart
