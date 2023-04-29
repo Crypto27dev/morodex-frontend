0ea45d0e05007c8c0f2059cb0090f20017a570c9
CryptoSnowPrince <topdirector2017@gmail.com>
2023-04-29 11:53:58 +0900

    update logo and text

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -69,16 +69,16 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
+          content="Morodex"
         />
         <meta name="theme-color" content="#1FC7D4" />
-        {/* <meta name="twitter:image" content="https://dapp-frontend-prince.web.app/images/hero.png" />
+        <meta name="twitter:image" content="https://morodex.org/images/check-your-network.png" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust."
+          content="Morodex"
         />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" /> */}
+        <meta name="twitter:card" content="https://morodex.org/images/check-your-network.png" />
+        <meta name="twitter:title" content="Morodex" />
         <meta name="twitter:image" content="" />
         <meta
           name="twitter:description"
